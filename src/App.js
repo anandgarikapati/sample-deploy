e763f5df74826fd724b9c889bce448aa91df0eae
anandgarikapati1 <anand.g@financebox.in>
2024-10-25 17:20:59 +0530

    second push

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>Anand</p>
+        <p>By Hacker Sai Kiran</p>
         <a
           className="App-link"
           href="https://reactjs.org"
